705704451d6debe2bd48f940ea6198a39db24944
laur80 <laur80raul@gmail.com>
2020-10-26 18:33:39 +0200

    Updates

---
+++ precache-manifest.dfdd78c24b10b06b78d971ddbef1529b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b276be3966196727a744d265ee86ef75",
+    "url": "/color-boxes/index.html"
+  },
+  {
+    "revision": "b5a6be01148d587d71d0",
+    "url": "/color-boxes/static/css/main.85add121.chunk.css"
+  },
+  {
+    "revision": "fe163f98e93a7dc6c06d",
+    "url": "/color-boxes/static/js/2.cc0144b6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/color-boxes/static/js/2.cc0144b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a6be01148d587d71d0",
+    "url": "/color-boxes/static/js/main.622d23b4.chunk.js"
+  },
+  {
+    "revision": "826120d944169a179a20",
+    "url": "/color-boxes/static/js/runtime-main.82e1872a.js"
+  }
+]);
